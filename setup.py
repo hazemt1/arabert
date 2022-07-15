001ecff375a3c91afaa44580c620cfeb599da97a
hazemt1 <45517271+hazemt1@users.noreply.github.com>
2022-07-15 19:06:33 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='arabert',
+    version='',
+    packages=['arabert', 'aragpt2', 'aragpt2.gpt2', 'aragpt2.grover', 'araelectra', 'araelectra.util',
+              'araelectra.model', 'araelectra.finetune', 'araelectra.pretrain'],
+    url='',
+    license='',
+    author='aub-mind',
+    author_email='',
+    description=''
+)
